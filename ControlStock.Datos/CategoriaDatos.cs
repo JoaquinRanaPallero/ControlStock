833b682dc5bc639fc2951d7fdd255ff4bff9654e
Joaquín Raña <joaquintomasrana@gmail.com>
2025-08-15 12:17:48 -0300

    se agregó clase CategoriaDatos

---
+++ ControlStock.Datos/CategoriaDatos.cs
@@ -0,0 +1,43 @@
+﻿using ControlStock.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStock.Datos
+{
+    public class CategoriaDatos
+    {
+        // Cadena de conexión tomada del App.config
+        private readonly string connectionString =
+            ConfigurationManager.ConnectionStrings["CatalogoDb"].ConnectionString;
+
+        // Método para listar todas las categorías
+        public List<Categoria> Listar()
+        {
+            var lista = new List<Categoria>();
+            const string sql = "SELECT Id, Descripcion FROM CATEGORIAS ORDER BY Descripcion";
+
+            using (var conexion = new SqlConnection(connectionString))
+            using (var comando = new SqlCommand(sql, conexion))
+            {
+                conexion.Open();
+                using (var lector = comando.ExecuteReader())
+                {
+                    while (lector.Read())
+                    {
+                        lista.Add(new Categoria
+                        {
+                            Id = (int)lector["Id"],
+                            Descripcion = lector["Descripcion"] as string
+                        });
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+}
---
+++ ControlStock.Negocio/CategoriaNegocio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlStock.Negocio
+{
+    public class CategoriaNegocio
+    {
+    }
+}
